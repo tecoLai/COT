be1e25b00eca1dc38a0ff28ccfcb61bdb5d1c4e3
lai.yah.shen <lai.yah.shen@tecotec.co.jp>
2018-02-08 19:03:31 +0900

    [deploy] add rinkeby test network info

--- truffle.js
+++ truffle.js
@@ -21,6 +21,12 @@ module.exports = {
       port: 8545,
       network_id: "15",
       from: "0x627306090abab3a6e1400e9345bc60c78a8bef57" //please change this to your address
-    } 
+    },
+    rinkeby: { // this will deploy to the rinkeby test network
+      host: "localhost",
+      port: 8545,
+      network_id: "4", //rinkeby default id
+      from: "0x627306090abab3a6e1400e9345bc60c78a8bef57" //please change this to your address
+    }      
   }
 };
